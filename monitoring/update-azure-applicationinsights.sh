546def7592f1db110098e795cdf0ceccb2d805d1
baardl <bard.lind@gmail.com>
2026-04-16 14:22:18 +0200

    Add monitoring via Azure Application Insights

--- monitoring/update-azure-applicationinsights.sh
+++ monitoring/update-azure-applicationinsights.sh
@@ -4,4 +4,4 @@ if [ -f "$SEMANTIC_PROPERTIES_FILE" ]; then
     . $SEMANTIC_PROPERTIES_FILE
 fi
 
-$(dirname $BASH_SOURCE)/semantic_update_service.sh --group-id "$GROUP_ID" --artifact-id "$ARTIFACT_ID"
+$(dirname $BASH_SOURCE)/semantic_update_service.sh --group-id="$GROUP_ID" --artifact-id="$ARTIFACT_ID"
